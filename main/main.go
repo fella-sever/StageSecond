827999b9f5924daa2296e3706a12bdc030639caf
Gorbovskyfallz <funkydrumma@yandex.ru>
2022-03-02 18:55:16 +0500

    SecondStageSecondDay

--- main/main.go
+++ main/main.go
@@ -1,27 +1,7 @@
 package main
 
 func main() {
-	fibonacci(2)
 
-}
-
-func fibonacci(n int) int {
-	var (
-		num1 int = 1
-		num2 int = 1
-		sum  int
-		i    int
-	)
-	if n == 1 || n == 2 {
-		return 1
-	}
-
-	for i = 2; i != n; i++ {
-		sum = num1 + num2
-		num1 = num2
-		num2 = sum
-
-	}
-	return sum
+	SumInt(1, 2, 3, 4, 5, 6, 7)
 
 }
