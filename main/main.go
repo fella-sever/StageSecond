ed31c082507ca21cb947d102194cd34456f67af8
Gorbovskyfallz <funkydrumma@yandex.ru>
2022-04-20 17:09:34 +0500

    chekingNetwork, clearBash, testinUSBCam

--- main/main.go
+++ main/main.go
@@ -1,7 +1,46 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os/exec"
+	"time"
+)
 
-	SumInt(1, 2, 3, 4, 5, 6, 7)
+func testin() {
+	cmd := exec.Command("lsusb")
+	stdout, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	// Print the output
+	fmt.Println(string(stdout))
+}
+func cleanBash() {
+	cmd := exec.Command("clear")
+	stdout, err := cmd.Output()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(string(stdout))
+}
+
+func checkinNetwork() {
+	cmd := exec.Command("ping", "-c 3", "1.1.1.1")
+	_, err := cmd.Output()
+	if err != nil {
+		fmt.Println("no ping")
+
+	}
+	//fmt.Println(string(stdout))
+	fmt.Println("ok")
+}
+
+func main() {
+	for {
+		time.Sleep(1 * time.Second)
+		checkinNetwork()
+		//time.Sleep(1 * time.Second)
+
+	}
 
 }
